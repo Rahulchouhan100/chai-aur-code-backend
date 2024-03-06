40ade0ea198fdbf4a74271bdb4151921d1b05bc7
Rahulchouhan100 <rahulchouhan8991@gmail.com>
2024-03-06 19:48:23 +0530

    string removed

--- backend/models/ecommerce/product.models.js
+++ backend/models/ecommerce/product.models.js
@@ -1,7 +1,35 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    
-}, { timestamps: true });
+const productSchema = new mongoose.Schema(
+  {
+    decription: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+
+    productImage: {
+      type: String,
+    },
+
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      default: 0,
+      type: Number,
+    },
+
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  { timestamps: true }
+);
 
 export const Product = mongoose.model("Product", productSchema);
--- backend/models/todos/todos.models.js
+++ backend/models/todos/todos.models.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const todoSchema = new mongoose.Schema(
   {
     content: {
-      type: string,
+      type: String,
       required: true,
     },
     complete: {
